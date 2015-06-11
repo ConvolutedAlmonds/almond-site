e98324c835e53d2628709ffa2c5214ebc2281bf6
Kyle Shockey <kyleshockey1@gmail.com>
2015-06-11 16:30:21 -0700

    gitignore updates, fix index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require('express'),
+var express = require('express');
 var app = express();
 var http = require('http');
 var httpServer = http.Server(app);
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/src'));
 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/index.html');
